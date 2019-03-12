f95c9244152d8a5e313c933a88459faf8d70d903
Maurício Ackermann <mabiancoac@gmail.com>
2019-03-12 14:12:40 -0300

    Updates

---
+++ precache-manifest.ce0df1e80d3f555da4c7d0be68699365.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c05eb4c587e73356b5ba",
+    "url": "/fernanda/static/css/main.fc406a6a.chunk.css"
+  },
+  {
+    "revision": "c05eb4c587e73356b5ba",
+    "url": "/fernanda/static/js/main.d75c82da.chunk.js"
+  },
+  {
+    "revision": "e2e1091024b7d4e80a5c",
+    "url": "/fernanda/static/js/runtime~main.02c6bc7c.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/fernanda/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "4fc4d8dfaa0394d8ebead59260cb531d",
+    "url": "/fernanda/index.html"
+  }
+];
